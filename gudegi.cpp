a8ad520a95ee555593232fc44388bd1e2f9a1e8c
CSE-Shaco <106949557+CSE-Shaco@users.noreply.github.com>
2023-04-03 09:39:14 +0900

    Create gudegi.cpp

---
+++ gudegi.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>/*l}#'8 $$\"+%xem%pbi;:*/
+using namespace std;const string a="=59965=192;"
+"36>2Ahoc}etvdbumjj{,%\"*\"7\"Yla|b-()=7=0<0:;8"
+";:?;86**Phhlik)tjep/#/`fle%gp%pbi\u007Fgb '#bf"
+"(clh/~ul'\"iwbl'wm,ml}#'8 $$\"+%xemwe!('$*";//x
+const int aa=37;int aaa(int aaaa, int aaaaa);int
+aaaaaa(int      aaaaaaa,int aaaaaaaa){if(aaaaaaa
+<=7+-7 ||         aaaaaaaa <= 0)return 1;return(
+aaaaaaa^19         +aaa(aaaaaaa -1,aaaaaaaa)+aaa
+(aaaaaaa  - 2,       aaaaaaaa -1) + 1) % aa;}int
+aaa (int  aaaa,       int aaaaa){if(aaaa <= 0 ||
+aaaaa<= 7- 3 -4)       return 0;return ( aaaaaa(
+aaaa-1, aaaaa- 1)       ^(aaaaaa(aaaa - 2, aaaaa
+) %(0x2 + 0x3)) ^(        aaaaaa(aaaa-3, aaaaa /
+(0x2 )) << 2)) %aa;}          int main(){for(int
+aaaaaaaaa= 0; aaaaaaaaa          < a .length() ;
+aaaaaaaaa++){int aaaaaaaaaa  = (aaa(aaaaaaaaa*10
+,aaaaaaaaa* 10) & (aaa(2, 2) - 1)); cout<<(char)
+(a [aaaaaaaaa] ^ aaaaaaaaaa); cout << flush;  if
+(aaaaaaaaa % 15 == 14) cout << endl;} return 0;}
+/* ;:?;86**Phhlik)tjep/#/`fle%gp%pbi\u007Fgb '{,
+;36>2Ahoc}etvdbumjj{,%\"*\"7\"Yla|b%\"*\"7hhli*/
